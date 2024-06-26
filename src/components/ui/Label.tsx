133be9a7cca347b180b9baeb2843f9aa912950eb
Luis Mindiola <lmindiola89@gmail.com>
2024-06-25 20:49:49 -0500

    first commit

---
+++ src/components/ui/Label.tsx
@@ -0,0 +1,12 @@
+interface Props extends React.LabelHTMLAttributes<HTMLLabelElement> {}
+
+export function Label({ children, ...props }: Props) {
+  return (
+    <label
+      className="block text-sm font-medium leading-6 text-gray-900"
+      {...props}
+    >
+      {children}
+    </label>
+  );
+}
---
+++ src/components/ui/index.tsx
@@ -0,0 +1,13 @@
+// Use en node
+
+// import { Input } from "@/components/ui/Input";
+// import { Label } from "@/components/ui/Label";
+// import { Button } from "@/components/ui/Button";
+
+// export { Input, Label, Button };
+
+// Use en ts
+
+export { Input } from "@/components/ui/Input";
+export { Label } from "@/components/ui/Label";
+export { Button } from "@/components/ui/Button";
